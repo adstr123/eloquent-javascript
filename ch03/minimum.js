f8e9b5cbc86d40b9ec6ed63fec6e28c95557484c
adstr123 <adam.matheson@ntlworld.com>
2020-03-23 13:25:21 +0000

    CH03

---
+++ ch03/minimum.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the minimum value of a and b
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export const minimum = (a, b) => (a < b ? a : b);
+
+console.log(minimum(22, -2));
