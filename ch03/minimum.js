c044b5f5877216b70587faf4c9b33abd764aa686
adstr123 <adam.matheson@ntlworld.com>
2020-04-14 23:29:56 +0100

    UNTRACKED FILES NEWLY ADDED TO .GITIGNORE

--- ch03/minimum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Returns the minimum value of a and b
- */
-exports.minimum = (a, b) => (a < b ? a : b);
-console.log(exports.minimum(22, -2));
